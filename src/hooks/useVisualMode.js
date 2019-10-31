bdbd2caccf2749a474e1811d4c7d4b5e3873718f
Araxiah <cewoo98@hotmail.com>
2019-10-31 14:04:24 -0400

    added back function in useVisualMode.js

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,9 +3,28 @@ import Show from "components/Appointment/Show"
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
 
-  return { mode,
-    transition: (mode) => setMode(mode) };
+  
+  const transition = function(mode){ 
+    setHistory(history => [mode, ...history ])
+    setMode(mode)    
   }
+
+  const back = function(){    
+    setHistory( prev => {
+      prev.splice(0, 1);
+      setMode(prev[0]);
+      return [ ...prev]
+    })
+  }
+
+  return { 
+    mode,
+    transition,
+    back
+  
+  };
+}
 
 
