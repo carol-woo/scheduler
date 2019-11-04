16d29b14dc6109aa57a0c624c4eece073e6849c2
Araxiah <cewoo98@hotmail.com>
2019-11-04 17:03:31 -0500

    Can mock an api call now

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,15 @@
 import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 import Application from "components/Application";
+import axios from "__mocks__/axios"
+
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
